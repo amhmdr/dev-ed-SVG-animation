75e36f73922d6d1e7a2836fcf818347d470f21fa
amhmdr <91362430+amhmdr@users.noreply.github.com>
2022-02-22 13:26:39 +0800

    fixed js code

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// select all paths in logo
-console.log("sadas");
-const logo = document.querySelectorAll(".logo");
-console.log(logo)
-
+const logo = document.querySelectorAll("#logo path");
+console.log(logo.length);
 for (let i = 0; i < logo.length; i++) {
-  console.log("sadas");
-  console.log(`Letter ${i} is ${logo[i].getTotalLength}`);
+  console.log("f");
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+/* 
+
+not working here for some reasomn. just copy svg to codepen html with id, and paste the above js and it works
+https://codepen.io/GitHuberian/pen/opGZjM?editors=1111
+
+or else use this js fiddle with jquery
+
+https://jsfiddle.net/n4ypt7bd/
+
+*/
